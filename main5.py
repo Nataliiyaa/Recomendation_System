15702436f3feca0aa317f2b0ab6af3027868753f
Nataliiyaa <agupovanatasha05@gmail.com>
2025-04-11 22:19:24 +0300

    базовая часть авторизации и аутентификации + фоновая работа синхронно и ассинхронно

---
+++ main5.py
@@ -0,0 +1,24 @@
+import asyncio
+import time
+from fastapi import FastAPI, BackgroundTasks
+
+
+app = FastAPI()
+
+
+def sync_task():
+    time.sleep(3)
+    print("Отправлен email")
+
+
+async def async_task():
+    await asyncio.sleep(3)
+    print("Сделан запрос в сторонний API")
+
+
+@app.post("/")
+async def some_route(bg_tasks: BackgroundTasks):
+    ...
+    # asyncio.create_task(async_task())
+    bg_tasks.add_task(sync_task)
+    return {"ok": True}
